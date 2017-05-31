19e3dc1236377de86147df8f3635fbbfe7558b2e
Adam Sokolnicki <adam.sokolnicki@gmail.com>
2017-05-31 17:51:45 +0200

    Bumb the version number

--- projectile-rails.el
+++ projectile-rails.el
@@ -4,7 +4,7 @@
 
 ;; Author:            Adam Sokolnicki <adam.sokolnicki@gmail.com>
 ;; URL:               https://github.com/asok/projectile-rails
-;; Version:           0.12.0
+;; Version:           0.15.0
 ;; Keywords:          rails, projectile
 ;; Package-Requires:  ((emacs "24.3") (projectile "0.12.0") (inflections "1.1") (inf-ruby "2.2.6") (f "0.13.0") (rake "0.3.2"))
 
