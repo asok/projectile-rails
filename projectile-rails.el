fc45a625cc8c99bd4f428c543a7fdc0230521cd6
Steve Purcell <steve@sanityinc.com>
2024-01-06 16:30:01 +0000

    Use standard keyword as suggested by package-lint

--- projectile-rails.el
+++ projectile-rails.el
@@ -5,7 +5,7 @@
 ;; Author:            Adam Sokolnicki <adam.sokolnicki@gmail.com>
 ;; URL:               https://github.com/asok/projectile-rails
 ;; Version:           0.22.0
-;; Keywords:          rails, projectile
+;; Keywords:          rails, tools, projectile
 ;; Package-Requires:  ((emacs "25.1") (projectile "0.12.0") (inflections "1.1") (inf-ruby "2.2.6") (f "0.13.0") (rake "0.3.2") (dash "2.18.1"))
 
 ;; This file is NOT part of GNU Emacs.
